1a97f906d1f30879a8fa968f6e438efe13bb7b63
dgbarr2 <dgbarr@mac.com>
2022-03-18 13:52:54 +0000

    Add files via upload

---
+++ 27_SettingYourWorkingDirectory/setMyWorkingDirectory.R
@@ -0,0 +1,17 @@
+#
+# setMyWorkingDirectory.R
+#
+# Small file to set the working directory.
+#
+# DGB April 2020
+#
+
+
+# Edit the following line to replace 320709 with our own Bank ID before running running this file. 
+# Then save it so that you don't have to edit it again.
+
+
+setwd("C:/Users/320709/Documents/FY_Rcourse/myCodeAndData")
+
+getwd()
+
